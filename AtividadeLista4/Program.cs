339298f264a3c37c2d66bd561a15af087cc57c65
Windows <Windows@DESKTOP-JAN2O6E>
2023-05-07 11:14:12 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadeLista4/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AtividadeLista4;
+int resposta;
+Console.WriteLine("Escolha a questão");
+resposta = int.Parse(Console.ReadLine());
+
+switch (resposta)
+{
+    case 1:
+        questao1.Executar();
+        break;
+}
+
+switch (resposta)
+{
+    case 2:
+        questao2.Executar();
+        break;
+}
+
+switch (resposta)
+{
+    case 3:
+        questao3.Executar();
+        break;
+}
+
+
+switch (resposta)
+{
+    case 4:
+        questao4.Executar();
+        break;
+}
+
+switch (resposta)
+{
+    case 5:
+        questao5.Executar();
+        break;
+}
+
+switch (resposta)
+{
+    case 6:
+        questao6.Executar();
+        break;
+}
+
+switch (resposta)
+{
+    case 7:
+        questao7.Executar();
+        break;
+}
+
+switch (resposta)
+{
+    case 8:
+        questao8.Executar();
+        break;
+}
+
+switch (resposta)
+{
+    case 9:
+        questao9.Executar();
+        break;
+}
+
+switch (resposta)
+{
+    case 10:
+        questao10.Executar();
+        break;
+}
